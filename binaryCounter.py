7cc64d56d69832a2d5a04da97d299a82931833af
Sama <sama.naraghi@gmail.com>
2019-07-22 13:32:56 +0200

    counts up with LEDs working

--- binaryCounter.py
+++ binaryCounter.py
@@ -10,13 +10,18 @@ Date: <21/07/2019>
 """
 
 # import Relevant Librares
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from itertools import product
 import time
 
 # public variables
 binaryValues = list(product(range(2), repeat=3))
 count = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 # Logic that you write
 def main():
@@ -42,6 +47,19 @@ def refreshLEDs():
     global binaryValues
     global count
     print(binaryValues[count]);
+    if (binaryValues[count][0] == 1):
+        GPIO.output(17,GPIO.HIGH)
+    else:
+        GPIO.output(17,GPIO.LOW)
+    if (binaryValues[count][1] == 1):
+        GPIO.output(27,GPIO.HIGH)
+    else:
+        GPIO.output(27,GPIO.LOW)
+    if (binaryValues[count][2] == 1):
+        GPIO.output(22,GPIO.HIGH)
+    else:
+        GPIO.output(22,GPIO.LOW)
+
 
 # Only run the functions if
 if __name__ == "__main__":
@@ -52,8 +70,8 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
-        # GPIO.cleanup()
+        GPIO.cleanup()
     except e:
-        # GPIO.cleanup()
+        GPIO.cleanup()
         print("Some other error occurred")
         print(e.message)
